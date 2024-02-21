dd80f37e3008034b5204eea631b78da1aa18c102
Joel Sunman <joelsunman@gmail.com>
2024-02-21 16:58:23 -0600

    refactor: re-enable google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-  modules: ['@vueuse/nuxt', '@pinia/nuxt', '@nuxt/test-utils/module', 'nuxt-security'],
+  modules: ['@vueuse/nuxt', '@pinia/nuxt', '@nuxt/test-utils/module', 'nuxt-security', 'nuxt-gtag'],
   security: {
     headers: {
       contentSecurityPolicy: {
