0e8bcd7d5652eb63d8172ddacd00e5f117d560b6
Joel Sunman <joelsunman@gmail.com>
2024-04-14 12:48:12 -0500

    deps updated

--- data/game-insert-sample.sql
+++ data/game-insert-sample.sql
@@ -1,3 +1,6 @@
 INSERT INTO artists ("artistId", name) VALUES
-    ('1anyVhU62p31KFi8MEzkbf', 'Metro Boomin')
+    ('2RQXRUsr4IW1f3mKyKsy4B', 'Noah Kahan')
     ON CONFLICT (name) DO NOTHING;
+
+update artists set artistId = '2m62cc253Xvd9qYQ8d2X3d', name = 'The Alan Parsons Project' where id = 483;
+ 
