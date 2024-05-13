586a56f8636caedc16390efb381df11d00ed08ad
Joel Sunman <joelsunman@gmail.com>
2024-05-12 20:19:35 -0500

    deps

--- data/game-insert-sample.sql
+++ data/game-insert-sample.sql
@@ -1,5 +1,5 @@
 INSERT INTO artists ("artistId", name) VALUES
-    ('2RQXRUsr4IW1f3mKyKsy4B', 'Noah Kahan')
+    ('3WrFJ7ztbogyGnTHbHJFl2', 'The Beatles')
     ON CONFLICT (name) DO NOTHING;
 
 update artists set artistId = '2m62cc253Xvd9qYQ8d2X3d', name = 'The Alan Parsons Project' where id = 483;
