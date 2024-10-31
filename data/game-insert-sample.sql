cdbc1a167f1f8b564111b64589dca24449c42bf1
Joel Sunman <joelsunman@gmail.com>
2024-10-30 20:29:55 -0500

    deps

--- data/game-insert-sample.sql
+++ data/game-insert-sample.sql
@@ -1,7 +1,7 @@
 INSERT INTO artists ("artistId", name) VALUES
-    ('7dGJo4pcD2V6oG8kP0tJRR', 'Eminem')
+    ('5DGJC3n9DS0Y9eY5ul9y0O', 'Marika Hackman')
     ON CONFLICT (name) DO NOTHING;
 
-update artists set "artistId" = '1Z8ODXyhEBi3WynYw0Rya6', name = 'De La Soul' where id = 567;
+update artists set "artistId" = '5DGJC3n9DS0Y9eY5ul9y0O', name = 'Marika Hackman' where id = 681;
 
 ALTER SEQUENCE artists_id_seq RESTART WITH 719;
