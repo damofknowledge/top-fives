8ab5bc72f9156c00e92eeb4f59cb266fa67112b4
Joel Sunman <joelsunman@gmail.com>
2025-10-03 21:21:18 -0500

    deps

--- data/game-insert-sample.sql
+++ data/game-insert-sample.sql
@@ -1,7 +1,8 @@
 INSERT INTO artists ("artistId", name) VALUES
-    ('6bUJpbekaIlq2fT5FMV2mQ', 'Wavves')
-    ON CONFLICT (name) DO NOTHING;
+    ('0kGweFvHWUfh6oLnookVeO', 'Cameron Winter');
 
-update artists set "artistId" = '0KydPQPUUoTNhmiHKOg5Er', name = 'Neon Indian' where id = 854;
+update artists set "artistId" = '57SxomZx6rGWH3aJMAJxiu', name = 'Peter Frampton'
+where id = 1083;
 
 ALTER SEQUENCE artists_id_seq RESTART WITH 719;
+
