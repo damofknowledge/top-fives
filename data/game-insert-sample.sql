8dd70792414754d7dbf949a63617517042d11f7c
Joel Sunman <joelsunman@gmail.com>
2025-12-12 19:46:51 -0600

    dev dependencies

--- data/game-insert-sample.sql
+++ data/game-insert-sample.sql
@@ -1,15 +1,10 @@
 INSERT INTO artists ("artistId", name) VALUES
-    ('7CCn4PFRRRZF127jtCBAUe', 'The Lovin’ Spoonful'),
-    ('1WHoAjAWGx5qLsgzpaOk7W', 'Hercules & Love Affair'),
-    ('74fkl73HDlCXw0l6cemB89', 'G. Love & Special Sauce'),
-    ('7mu52yh5bBhhJOyVndAEZW', 'Cleopatra'),
-    ('6bWxFw65IEJzBYjx3SxUXd', 'Morcheeba');
+    ('46gyXjRIvN1NL1eCB8GBxo', 'All Time Low'),
+    ('0DccOPSJ5LcC4HDiDZl17u', 'Lowdown Brass Band');
 
-update artists set "artistId" = '23rleGXVOVVgTk3xgtmfE4', name = 'Gordon Lightfoot' where id = 1082;
+update artists set "artistId" = '2urZrEdsq72kx0UzfYN8Yv', name = 'Spandau Ballet' where id = 1104;
 
 ALTER SEQUENCE artists_id_seq RESTART WITH 719;
 
- 4HLWX9kzfraiCsTvrxJEXm | The Chameleons              
- 2c0qomdZabcCyqTZ5M9msd | The Donnas  
- 2K5IFQpKad0Dpi7JeAj8D4 | ANIMATEDGIF | 1082
+
 
