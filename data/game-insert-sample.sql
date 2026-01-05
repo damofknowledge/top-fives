7d60b5d7215be86f060b2e613a7a63ba1869e621
Joel Sunman <joelsunman@gmail.com>
2026-01-04 21:24:01 -0600

    deps

--- data/game-insert-sample.sql
+++ data/game-insert-sample.sql
@@ -1,7 +1,10 @@
-INSERT INTO artists ("artistId", name, id) VALUES
-    ('5mFKYdmiYwNJTDtSzgFyQx', 'Andy Shauf', 1246),
-    ('6hnWRPzGGKiapVX1UCdEAC', 'Sam Cooke', 1247),
-    ('38euc5ce2q2MMzaoJcPstz', 'Josh Rouse', 1248);
+INSERT INTO artists ("artistId", name) VALUES
+    ('1BH45DVSTeGBvcYXNCd67g', 'M.O.P.'),
+    ('4tuJ0bMpJh08umKkEXKUI5', 'Gracie Abrams'),
+    ('0O5sXbPJPWbHKnbLfdLARS', 'Beulah'),
+    ('0WKIRXNWr6cYlm8ZYVXD1I', 'Charo'),
+    ('3IYUhFvPQItj6xySrBmZkd', 'Creedence Clearwater Revival'),
+    ('4VMYDCV2IEDYJArk749S6m', 'Daddy Yankee');
 
 update artists set "artistId" = '6bI8H2TnlKYGJSo52wcTP4', name = 'Taylor Hawkins' where id = 1120;
 
