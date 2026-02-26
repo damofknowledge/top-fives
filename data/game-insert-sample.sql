cbf91a5560a262baed60c19597f2003e94da854f
Joel Sunman <joelsunman@gmail.com>
2026-02-25 20:54:34 -0600

    deps

--- data/game-insert-sample.sql
+++ data/game-insert-sample.sql
@@ -1,12 +1,6 @@
 INSERT INTO artists ("artistId", name) VALUES
-    ('1BH45DVSTeGBvcYXNCd67g', 'M.O.P.'),
-    ('4tuJ0bMpJh08umKkEXKUI5', 'Gracie Abrams'),
-    ('0O5sXbPJPWbHKnbLfdLARS', 'Beulah'),
-    ('0WKIRXNWr6cYlm8ZYVXD1I', 'Charo'),
-    ('3IYUhFvPQItj6xySrBmZkd', 'Creedence Clearwater Revival'),
-    ('4VMYDCV2IEDYJArk749S6m', 'Daddy Yankee');
+    ('', '');
 
-update artists set "artistId" = '6bI8H2TnlKYGJSo52wcTP4', name = 'Taylor Hawkins' where id = 1120;
+update artists set "artistId" = '4q3ewBCX7sLwd24euuV69X', name = 'Bad Bunny (Super Bowl LX Edition)' where id = 1172;
 
 ALTER SEQUENCE artists_id_seq RESTART WITH 719;
-
